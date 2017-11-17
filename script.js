20e4858f9bf0edd0029ef27a76ca9bc3c128cf0f
Mimi Chang <mimichang94@yahoo.com>
2017-11-17 18:57:01 +0000

    fix mobile responsiveness

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-$(function() {
-
-	$("#contactForm").click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
-		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$("#submitButton").click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
-		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
+//$("#submitButton").on("click", function() {
