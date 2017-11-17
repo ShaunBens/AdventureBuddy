fcb59a73e605f417223c33acbe65a953c33fcc5e
Eddie Pedersen <eddiepedersen90@gmail.com>
2017-11-17 19:16:33 +0000

    formFadeOut

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-$(function() {
+console.log("Script connected");
 
-	$("#contactForm").click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
-		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$("#submitButton").click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
-		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
-});
-
+$("#submitButton").on("click", function(event) {
+    event.preventDefault();
+    //grab name & number
+    $("#contact").hide();
+})
